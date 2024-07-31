5e2badbbb3eabb742e8dea8e5930c92f6b4994a0
kmaparna <aparna14km@gmail.com>
2024-07-31 19:43:01 +0530

    Circle Area

--- basics.js
+++ basics.js
@@ -1,5 +1,6 @@
 var n = 10;
 
+/* Addition */
 function AdditionofNumbers() {
   var m = 20;
   var sum = n + m;
@@ -9,3 +10,30 @@ function AdditionofNumbers() {
 AdditionofNumbers();
 
 console.log("Value of n is: " + n);
+
+/* Area */
+function calculateArea(length, width){
+let result;
+result = length * width;
+return result;
+}
+
+console.log('Calculate Area', calculateArea(8,2));
+
+/* Calculate Area of Circle given radius */
+function calculateCircleAreaWithRadius(radius){
+  const pie = 3.14;
+  let result = pie * (radius * radius);
+  return result;
+  }
+  
+console.log('Calculate circle Area with Radius', calculateCircleAreaWithRadius(2));
+
+/* Calculate Area of Circle given diameter */
+function calculateCircleArea(diameter){
+  const pie = 3.14;
+  let result = pie * (diameter);
+  return result;
+  }
+  
+console.log('Calculate circle Area with Diameter', calculateCircleArea(4));
